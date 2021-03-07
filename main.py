3430565c6dd6d014cf90b318aba15c8d7c25c0ae
swill <swilltec@gmail.com>
2021-03-07 09:56:36 -0500

    added update

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from blog import schemas, models
 from blog.database import engine, SessionLocal
 
 app = FastAPI()
-
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = SessionLocal()
@@ -20,8 +20,8 @@ def get_db():
 
 
 @app.post('/blog/', status_code=status.HTTP_201_CREATED)
-def create_blogpost(blog: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=blog.title, body=blog.body)
+def create_blogpost(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
@@ -44,11 +44,26 @@ def get_blogpost_by_id(id, response:Response, db: Session = Depends(get_db)):
     return blog
 
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blogpost_by_id(id, request: schemas.Blog, db: Session=Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail= f'blog post with this id {id} does not exist')
+    blog.update(request)
+    db.commit()
+    
+    return {'msg':'operation was sucessful'}
+
+
 @app.delete('/blog/{id}', status_code=status.HTTP_200_OK)
 def delete_blogpost_by_id(id, response:Response, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).\
-            delete(synchronize_session=False)
+    blog =  db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail= f'blog post with this id {id} does not exist')
 
+    blog.delete(synchronize_session=False)
     db.commit()
   
     return {'msg':'operation was sucessful'}
