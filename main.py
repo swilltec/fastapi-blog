8345f75dba51381c536996233bac28cb14f80bb3
swill <swilltec@gmail.com>
2021-03-07 12:28:14 -0500

    added get user by id

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 
 from sqlalchemy.orm import Session
 
+
 from blog import schemas, models
 from blog.database import engine, SessionLocal
+from blog.hashing import get_password_hash
 
 app = FastAPI()
 models.Base.metadata.create_all(engine)
@@ -69,10 +71,21 @@ def delete_blogpost_by_id(id, response:Response, db: Session = Depends(get_db)):
   
     return {'msg':'operation was sucessful'}
 
-@app.post('/user/')
+@app.post('/user/', response_model = schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    hashed_password = get_password_hash(request.password)
+    new_user = models.User(name=request.name, email=request.email, password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def get_user_by_id(id: int, response:Response, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail= f'User with this id {id} does not exist')
+  
+    return user
