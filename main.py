ed19cff88a421de7fda83c7bd5d091de9d42db41
swill <swilltec@gmail.com>
2021-03-07 09:03:54 -0500

    added get post by id

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 
 from sqlalchemy.orm import Session
 
@@ -19,8 +19,8 @@ def get_db():
         db.close()
 
 
-@app.post('/blog/')
-def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
+@app.post('/blog/', status_code=status.HTTP_201_CREATED)
+def create_blogpost(blog: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
     db.commit()
@@ -29,6 +29,16 @@ def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
 
 
 @app.get('/blog/')
-def get_all(db: Session = Depends(get_db)):
+def get_all_blogpost(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get_blogpost_by_id(id, response:Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail= f'blog post with this id {id} does not exist')
+  
+    return blog
